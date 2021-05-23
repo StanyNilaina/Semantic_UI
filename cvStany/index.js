54bb32b71425b6b04747c5e13d52db322b3c4dec
StanyNilaina <stanyrazafindrakototo@gmail.com>
2021-05-23 22:39:10 +0300

    CvSemanticUi

---
+++ cvStany/index.js
@@ -0,0 +1,10 @@
+$(document).ready(()=>{
+	$('#html').progress({percent:97});$('#node').progress({percent:80});$('#js').progress({percent:90});
+	$('#bt').progress({percent:85});$('#ion').progress({percent:90});$('#ng').progress({percent:95});
+	$('#sui').progress({percent:97});$('#psd').progress({percent:90});$('#dsg').progress({percent:75});
+	$('#php').progress({percent:75});$('#fil').progress({percent:94});
+	$('#nest').progress({percent:85});
+
+
+})
+	
